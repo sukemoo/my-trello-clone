4d70db7130254ce3f9d77df7dafb0abd632cbe76
EC2 Default User <ec2-user@ip-172-31-42-34.ap-northeast-1.compute.internal>
2020-03-06 01:43:03 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import TrelloList from './TrelloList';
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    const { lists } = this.props;
+    return(
+      <div className="App">
+        <h2>Hello Kogane</h2>
+        <div style={styles.listsContainer}>
+          { lists.map(list => (
+            <TrelloList title={list.title} cards={list.cards} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  listsContainer: {
+    display: "flex",
+    flexDirection: "row"
+  }
+}
+
+const mapStateToProps = state => ({
+  lists: state.myLists
+});
+
+export default connect(mapStateToProps) (App);
